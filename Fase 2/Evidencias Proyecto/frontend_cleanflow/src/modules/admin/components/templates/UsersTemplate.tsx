52ec7558317414b4e8da82a618cdb4678fffaab8
TwoThousand-Four <jeremygarrido54@gmail.com>
2025-11-14 11:36:17 -0300

    Template para la vista de Gestión de usuarios

---
+++ Fase 2/Evidencias Proyecto/frontend_cleanflow/src/modules/admin/components/templates/UsersTemplate.tsx
@@ -0,0 +1,26 @@
+import UsersTable from "../organisms/UsersTable";
+import type { User } from "../../../types/user";
+
+interface Props {
+    users: User[];
+    loading: boolean;
+    onToggleActive: (id: number, active: boolean) => void;
+    onDelete: (id: number) => void;
+    }
+
+    const UsersTemplate: React.FC<Props> = ({ users, loading, onToggleActive, onDelete }) => {
+    return (
+        <div className="p-6">
+        <h1 className="text-2xl font-bold mb-6">Gestión de Usuarios</h1>
+
+        <UsersTable
+            users={users}
+            loading={loading}
+            onToggleActive={onToggleActive}
+            onDelete={onDelete}
+        />
+        </div>
+    );
+};
+
+export default UsersTemplate;
