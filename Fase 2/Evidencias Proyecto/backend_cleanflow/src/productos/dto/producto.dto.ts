8cf2227da7dbe4768a99302e772318945cff2239
Lucas Fernandez <lucasfernandez.mu@gmail.com>
2025-11-16 23:38:51 -0300

    Modificación campos DTO Producto

--- Fase 2/Evidencias Proyecto/backend_cleanflow/src/productos/dto/producto.dto.ts
+++ Fase 2/Evidencias Proyecto/backend_cleanflow/src/productos/dto/producto.dto.ts
@@ -1,6 +1,6 @@
 import { Categoria } from '../../categorias/entities/categoria.entity';
 import { Marca } from '../../marcas/entities/marca.entity';
-import { IsNotEmpty, IsNumber, IsOptional, IsString, Min } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString, Min } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateProductoDto {
@@ -22,7 +22,7 @@ export class CreateProductoDto {
     descripcionProducto?: string;
 
     @ApiProperty({ example: true, description: 'Indica si el producto está activo o no' })
-    @IsOptional()
+    @IsBoolean( { message: 'El estado del producto debe ser un booleano'})
     @IsNotEmpty({ message: 'El estado del producto no puede estar vacío' })
     productoActivo: boolean;
 
@@ -69,17 +69,11 @@ export class UpdateProductoDto{
     @IsString({ message: 'El nombre del producto debe ser una cadena de texto' })
     nombreProducto?: string;
 
-    @ApiProperty({ example: 1500, description: 'Precio del producto', required: false })
+    @ApiProperty({ example: 1500, description: 'Precio de compra del producto' })
     @IsOptional()
-    @IsNumber({}, { message: 'El precio del producto debe ser un número' })
-    @Min(0, { message: 'El precio del producto no puede ser negativo' })
-    precioProducto?: number;
-
-    @ApiProperty({ example: 2000, description: 'Precio de venta del producto', required: false })
-    @IsOptional()
-    @IsNumber({}, { message: 'El precio de venta del producto debe ser un número' })
-    @Min(0, { message: 'El precio de venta del producto no puede ser negativo' })
-    precioVentaProducto?: number;
+    @IsNumber({}, { message: 'El precio de compra del producto debe ser un número' })
+    @Min(0, { message: 'El precio de compra del producto no puede ser negativo' })
+    precioCompraProducto?: number;
 
     @ApiProperty({ example: 'Producto desinfectante para uso doméstico', description: 'Descripción del producto', required: false })
     @IsOptional()
