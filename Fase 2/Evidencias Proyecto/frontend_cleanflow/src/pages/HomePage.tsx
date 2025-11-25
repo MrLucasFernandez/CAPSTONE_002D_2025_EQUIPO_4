9df166c9289e5686dd2d1f7a18f16ee3ce5cb5c2
TwoThousand-Four <jeremygarrido54@gmail.com>
2025-11-24 22:38:14 -0300

    Home actualizado

--- Fase 2/Evidencias Proyecto/frontend_cleanflow/src/pages/HomePage.tsx
+++ Fase 2/Evidencias Proyecto/frontend_cleanflow/src/pages/HomePage.tsx
@@ -5,6 +5,9 @@ import slider1 from '@assets/imgs/slider1.png';
 import slider2 from '@assets/imgs/slider2.png';
 import slider3 from '@assets/imgs/slider3.png';
 
+// IMPORTAMOS LA SECCIÓN DE CATEGORÍAS
+import CategoriesSection from '@modules/categories/organisms/CategorySection';
+
 const dummySlides = [
   { id: 1, src: slider1, alt: 'Slide 1' },
   { id: 2, src: slider2, alt: 'Slide 2' },
@@ -13,8 +16,23 @@ const dummySlides = [
 
 const HomePage: React.FC = () => {
   return (
-    <div className="p-8 bg-gray-100 min-h-screen">
-      <ImageCarousel slides={dummySlides} />
+    <div className="bg-gray-100 min-h-screen">
+      
+      {/* Slider */}
+      <div className="pt-4 pb-10 px-4">
+        <ImageCarousel slides={dummySlides} />
+      </div>
+      
+      {/* Categorías destacadas */}
+      <CategoriesSection />
+
+      {/* Aquí luego agregamos: 
+          - Productos destacados
+          - Banner
+          - Por qué elegirnos
+          - Marcas 
+      */}
+
     </div>
   );
 };
