3bc926fdd91b0151e021f96640f4f8d0eeefbb20
TwoThousand-Four <jeremygarrido54@gmail.com>
2025-12-09 21:53:45 -0300

    ajuste del ignore

--- Fase 2/Evidencias Proyecto/frontend_cleanflow/dev-dist/sw.js
+++ Fase 2/Evidencias Proyecto/frontend_cleanflow/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ihmb10u56o8"
+    "revision": "0.u405l10546g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
